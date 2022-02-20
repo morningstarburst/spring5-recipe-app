50427a7ebd5ba5a10e834ed091f6134ce470bb6f
morningstarburst <2bhgk8derd@snkmail.com>
2022-02-19 23:31:28 -0600

    Finished bootstrapping recipes.

--- src/main/java/guru/springframework/bootstrap/DataLoader.java
+++ src/main/java/guru/springframework/bootstrap/DataLoader.java
@@ -43,20 +43,16 @@ public class DataLoader implements CommandLineRunner {
         Category mexicanCategory = this.categoryFinder("Mexican");
 
         Recipe perfectGuacamole = this.guacamoleRecipeBuilder(uomMap);
+        Recipe tacos = this.spicyGrilledChickenTacosRecipeBuilder(uomMap);
     }
 
     private Recipe guacamoleRecipeBuilder(Map<String, UnitOfMeasure> uomMap) {
         Recipe perfectGuacamole = new Recipe();
         UnitOfMeasure uomTeaspoon = uomMap.get("Teaspoon");
         UnitOfMeasure uomTablespoon = uomMap.get("Tablespoon");
-        UnitOfMeasure uomCup = uomMap.get("Cup");
         UnitOfMeasure uomPinch = uomMap.get("Pinch");
-        UnitOfMeasure uomOunce = uomMap.get("Ounce");
         UnitOfMeasure uomVoid = uomMap.get("");
-        UnitOfMeasure uomClove = uomMap.get("Clove");
-        UnitOfMeasure uomPint = uomMap.get("Pint");
 
-        // id will be generated
         perfectGuacamole.setDescription("Perfect guacamole");
         perfectGuacamole.setPrepTime(10);
         perfectGuacamole.setCookTime(0);
@@ -88,23 +84,63 @@ public class DataLoader implements CommandLineRunner {
         guacNotes.setRecipe(perfectGuacamole);
         perfectGuacamole.setNotes(guacNotes);
 
-        return perfectGuacamole;
+        return recipeRepository.save(perfectGuacamole);
     }
 
-    private Recipe spicyGrilledChickenTacosRecipeBuilder() {
-//        private Long id;
-//        private String description;
-//        private Integer prepTime;
-//        private Integer cookTime;
-//        private Integer servings;
-//        private String source;
-//        private String url;
-//        private String directions;
-//        private Set<Ingredient> ingredients;
-//        private Byte[] image;
-//        private Difficulty difficulty;
-//        private Notes notes;
-        return null;
+    private Recipe spicyGrilledChickenTacosRecipeBuilder(Map<String, UnitOfMeasure> uomMap) {
+        Recipe tacos = new Recipe();
+        UnitOfMeasure uomTeaspoon = uomMap.get("Teaspoon");
+        UnitOfMeasure uomTablespoon = uomMap.get("Tablespoon");
+        UnitOfMeasure uomCup = uomMap.get("Cup");
+        UnitOfMeasure uomPinch = uomMap.get("Pinch");
+        UnitOfMeasure uomOunce = uomMap.get("Ounce");
+        UnitOfMeasure uomVoid = uomMap.get("");
+        UnitOfMeasure uomClove = uomMap.get("Clove");
+        UnitOfMeasure uomPint = uomMap.get("Pint");
+
+        tacos.setDescription("Spicy Grilled Chicken Tacos");
+        tacos.setPrepTime(20);
+        tacos.setCookTime(15);
+        tacos.setServings(4);
+        tacos.setSource("Sally Vargas");
+        tacos.setUrl("https://www.simplyrecipes.com/recipes/spicy_grilled_chicken_tacos/");
+        tacos.setDirections(
+                "1. Prepare a gas or charcoal grill for medium-high, direct heat.\n" +
+                        "2. Make the marinade and coat the chicken.\n" +
+                        "3. Grill the chicken\n" +
+                        "4. Warm the tortillas\n" +
+                        "5. Assemble the tacos"
+        );
+        Set<Ingredient> ingredients = new HashSet<>();
+        ingredients.add(new Ingredient("Ancho Chili Powder", BigDecimal.valueOf(2), uomTablespoon));
+        ingredients.add(new Ingredient("Dried Oregano", BigDecimal.ONE, uomTeaspoon));
+        ingredients.add(new Ingredient("Dried Cumin", BigDecimal.ONE, uomTeaspoon));
+        ingredients.add(new Ingredient("Sugar", BigDecimal.ONE, uomTeaspoon));
+        ingredients.add(new Ingredient("Salt", BigDecimal.valueOf(0.5), uomTeaspoon));
+        ingredients.add(new Ingredient("Garlic, Finely Chopped", BigDecimal.ONE, uomClove));
+        ingredients.add(new Ingredient("Finely Grated Orange Zest", BigDecimal.ONE, uomTablespoon));
+        ingredients.add(new Ingredient("Fresh Squeezed Orange Juice", BigDecimal.valueOf(3), uomTablespoon));
+        ingredients.add(new Ingredient("Olive Oil", BigDecimal.valueOf(2), uomTablespoon));
+        ingredients.add(new Ingredient("Skinless, Boneless Chicken Thighs", BigDecimal.valueOf(6), uomVoid));
+        ingredients.add(new Ingredient("Small Corn Tortillas", BigDecimal.valueOf(8), uomVoid));
+        ingredients.add(new Ingredient("Packed Baby Arugula", BigDecimal.valueOf(3), uomCup));
+        ingredients.add(new Ingredient("medium ripe avocados, sliced", BigDecimal.valueOf(2), uomVoid));
+        ingredients.add(new Ingredient("radishes, thinly sliced", BigDecimal.valueOf(4), uomVoid));
+        ingredients.add(new Ingredient("cherry tomatoes, halved", BigDecimal.valueOf(0.5), uomPint));
+        ingredients.add(new Ingredient("red onion, thinly sliced", BigDecimal.valueOf(0.25), uomVoid));
+        ingredients.add(new Ingredient("Roughly chopped cilantro", BigDecimal.ONE, uomVoid));
+        ingredients.add(new Ingredient("sour cream", BigDecimal.valueOf(0.5), uomCup));
+        ingredients.add(new Ingredient("milk", BigDecimal.valueOf(0.25), uomCup));
+        ingredients.add(new Ingredient("lime, cut into wedges", BigDecimal.ONE, uomVoid));
+        tacos.setIngredients(ingredients);
+        tacos.setImage(null);
+        tacos.setDifficulty(Difficulty.KIND_OF_HARD);
+        Notes tacoNotes = new Notes();
+        tacoNotes.setRecipeNotes("Be careful handling chilis! If using, it's best to wear food-safe gloves. If no gloves are available, wash your hands thoroughly after handling, and do not touch your eyes or the area near your eyes for several hours afterwards.");
+        tacoNotes.setRecipe(tacos);
+        tacos.setNotes(tacoNotes);
+
+        return recipeRepository.save(tacos);
     }
 
     private Map<String, UnitOfMeasure> uomMapByDescriptionBuilder() {
